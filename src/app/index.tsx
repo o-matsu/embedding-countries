a41c2003529ca9a4bcac91f70e588578011a339c
o-matsu <matsushita.ryosuke@pine-app.studio>
2024-10-15 02:17:19 +0900

    build and deploy

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -17,7 +17,7 @@ export default function Index({ onSubmit }: Props) {
         <QueryForm query="" onSubmit={onSubmit} />
       </div>
       <div className="mt-6">
-        <Globe />
+        <Globe active={null} data={null} />
       </div>
       <div className="my-6 mx-12">
         <p className="text-gray-400 text-sm">G20とは、G7（フランス、米国、英国、ドイツ、日本、イタリア、カナダ（G7の議長国順）及び欧州連合（EU）に加え、アルゼンチン、豪州、ブラジル、中国、インド、インドネシア、メキシコ、韓国、ロシア、サウジアラビア、南アフリカ、トルコ（アルファベット順）及びアフリカ連合（AU）が参加する枠組です。G20の会議には、G20メンバー以外の招待国や国際機関などが参加することもあります。</p>
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -6,7 +6,7 @@ export default function Loading() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-black text-white">
       <div className="absolute ">
-        <Globe />
+        <Globe active={null} data={null} />
       </div>
       <div className="animate-spin rounded-full h-96 w-96 border-b-2 border-white"></div>
     </div>
